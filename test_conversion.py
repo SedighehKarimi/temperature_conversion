b08d4b995d2ff841cc498bd65679c9fba99ec56c
kianakarimi <sedigheh.karimi94@gmail.com>
2023-06-22 10:11:47 +0200

    add first test

---
+++ test_conversion.py
@@ -0,0 +1,6 @@
+from conversion import celsius_to_fahrenheit
+
+def test_clesius_to_fahrenheit() :
+    assert clesius_to_fahrenheit(20) = 68
+    assert clesius_to_fahrenheit(30) = 86
+    assert clesius_to_fahrenheit(0) = 32
